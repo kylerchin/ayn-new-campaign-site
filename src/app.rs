f5a1d513daf92cfc5fc1e979f9fb8874fd35a868
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-07-16 17:33:06 -0700

    Add social links, adjust footer distance

--- src/app.rs
+++ src/app.rs
@@ -25,7 +25,20 @@ pub fn Navbar() -> impl IntoView {
 
                     <a href="/events">"Events"</a>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSdZ1mzVYlWSv3M_wOI3q5YlQZeN22NKNVAZ9Hf47ueARc-cig/viewform">"Volunteer"</a>
+                 
+                   <a href="https://www.instagram.com/ayn4irvine/" class="h-8 w-8" >
+
+                   <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewbox="0 0 24 24"><title>instagram</title><path d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" /></svg>
+ 
+                   </a>
+                   <a href="https://www.facebook.com/profile.php?id=61554320496053" class="h-8 w-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewbox="0 0 24 24"><title>facebook</title><path d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" /></svg>
+                   </a>
+                   
                     <a href="https://www.efundraisingconnections.com/c/AynCraciun">
+
+                    
+
                 <div class="font-bold bg-octaorange text-white px-3 py-1.5 rounded-md">"Donate"</div>
                 </a>
                 </div>
@@ -188,7 +201,7 @@ fn EndorsementPage() -> impl IntoView {
 
             <li>"RISE (Remake Irvine Streets for Everyone)"</li>
 
-            <p class="text-octaorange text-lg">"Community Members"</p>
+            <p class="text-octaorange text-lg">"Community Leaders"</p>
             <li>"Branda Lin, City of Irvine Planning Commissioner and Irvine Watchdog Co-founder"</li>
 
             <li>"Dr. Jessica Pratt, UCI professor and Irvine resident"</li>
@@ -396,6 +409,9 @@ fn EventsPage() -> impl IntoView {
 
                 <h1 class="text-lg md:text-2xl text-octaorange font-bold">"Events"</h1>
 
+                <br/>
+                <h1 class="text-lg md:text-2xl text-octaorange font-bold">"Previous Events"</h1>
+
                 <p class="text-octablue font-bold">"Sunday, July 21"</p>
 
                 <img src="/assets/eventturtlerock.jpg" alt="Join us to meet Ayn Craciun, an excellent candidate to represent Turtle Rock. Refreshments provided." class="max-w-sm"/>
@@ -421,7 +437,7 @@ fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
 
     view! {
-        <Navbar/>
+        <div class="flex flex-col h-full"><Navbar/>
         <div
        class="mainhero w-full bg-center text-white bg-cover py-32 md:py-64"
         >
@@ -445,7 +461,7 @@ fn HomePage() -> impl IntoView {
         <WhyAynHome/>
         <EndorsementHome/>
 
-        <Footer/>
+        <Footer/></div>
     }
 }
 
